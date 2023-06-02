ba5231869fb17f7a36133a503065076c031e3731
Gopi K <k.gopi1@gmail.com>
2023-06-02 19:35:19 +0100

    added the tf version

--- infra/azure-reources.tf
+++ infra/azure-reources.tf
@@ -1,32 +1,4 @@
-resource "azurerm_resource_group" "azure" {
-  name     = "rg-${var.appname}"
-  location = "east us"
-}
-
-resource "azurerm_service_plan" "azure" {
-  name                = "asp--${var.appname}"
-  resource_group_name = azurerm_resource_group.azure.name
-  location            = azurerm_resource_group.azure.location
-  os_type             = "Windows"
-  sku_name            = "P1v2"
-}
-
-resource "azurerm_application_insights" "azure" {
-  name                = "ai-${var.appname}"
-  location            = azurerm_resource_group.azure.location
-  resource_group_name = azurerm_resource_group.azure.name
-  application_type    = "web"
-}
-
-resource "azurerm_app_service" "azure" {
-  name                = "app-${var.appname}"
-  location            = azurerm_resource_group.azure.location
-  resource_group_name = azurerm_resource_group.azure.name
-  app_service_plan_id = azurerm_service_plan.azure.id
-
-  site_config {  }
-
-  app_settings = { 
-    "APPINSIGHTS_INSTRUMENTATIONKEY" = "${azurerm_application_insights.azure.instrumentation_key}"
-  }
+resource "azurerm_resource_group" "resource_group" {
+  name = var.resource_group
+  location = var.location
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,10 @@
-variable "appname" {
+variable "resource_group" {
   type = string
+  description = "Resource Group"
 }
 
+variable "location" {
+  type = string
+  description = "Location"
+}
 
