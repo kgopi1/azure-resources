5f59f9d0dc38fcdf7443f115c18db530449ec126
Gopi K <k.gopi1@gmail.com>
2023-06-02 16:47:13 +0100

    added the gh script

---
+++ infra/variables.tf
@@ -0,0 +1,5 @@
+variable "appname" {
+  type = string
+}
+
+
---
+++ scripts/github_repo.sh
@@ -0,0 +1,57 @@
+## gihub repo create 
+
+
+gh_repo_create() {
+    # check repo exist
+    check_gh_repo(){
+        gh repo view gopi123
+        check_repo=$(gh repo view $repoName)
+        if [ $? != 0 ]; then # if repo doesnt exist. 
+             echo "gh repo needs to created"
+            gh repo create $repoName --public 
+        else
+           echo "repo exists github already "
+        fi
+    }
+    check_gh_repo
+
+}
+
+
+gh_login() {
+    gh_auth_status=$(gh auth status)
+    if [ $? == 0 ]; then
+        echo "github logged in already "
+    else
+        echo "gh login required"
+        gh auth login --with-token 
+        #gh auth login --with-token <token.txt
+    fi
+    
+}
+
+# az_login() {
+#     az_login_status=$(az account show)
+#     if [ $? == 0 ]; then
+#         echo "logged in"
+#     else
+#         echo "az login required"
+#         az login --use-device-code
+#     fi
+    
+# }
+
+# main
+repoName=$1
+gh_login
+gh_repo_create $repoName
+
+
+
+
+
+
+
+
+
+
